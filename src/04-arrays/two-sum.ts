a315528dbd32c7f4af5d67261269e1916d4c0a0e
Hemant Bisht <bishthemant169@gmail.com>
2026-08-16 22:41:46 +0530

    2 sum practice

--- src/04-arrays/two-sum.ts
+++ src/04-arrays/two-sum.ts
@@ -32,6 +32,20 @@ function twoSum(nums: number[], target: number): number[] {
 }
 
 
+function twoSum2(nums: number[], target:number): number[]{
+  const map = new Map<number, number>();
+
+  for(let i=0; i<nums.length; i++){
+    const complement = target - nums[i];
+    if(map.has(complement)){
+      return [map.get(complement)! , i]
+    }
+    map.set(nums[i], i);
+  }
+  return []
+
+}
+
 // ---------- TESTS ----------
 
 function runTests() {
--- src/16-binary-search/search-rotated-array.ts
+++ src/16-binary-search/search-rotated-array.ts
@@ -13,8 +13,39 @@
 // TODO: Implement this function
 function search(nums: number[], target: number): number {
   // Your code here
-  throw new Error("Not implemented");
-}
+  let low=0;
+    let high=nums.length-1;
+
+    let ans=-1;
+
+    while(low<=high){
+        let mid = Math.floor((low+high)/2);
+        if(nums[mid]===target){
+            ans=mid;
+            break;
+        }
+        if(nums[low]<=nums[mid]){
+          // left side is sorted
+          if(target>=nums[low] && target<nums[mid]){
+            high=mid-1;
+          }
+          else{
+            low=mid+1;
+          }
+        }
+        else{
+          // right side is sorted
+          if(target>nums[mid] && target<=nums[high]){
+            low=mid+1;
+          }
+          else{
+            high=mid-1;
+          }
+        }
+    }
+    return ans;
+        }
+
 
 // ---------- TESTS ----------
 
